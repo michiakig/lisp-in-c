15fd1f2e60a79c844b5d5e52ec218d942d9dc3f3
Michiaki Yamada <spacemanaki@gmail.com>
2011-03-04 01:25:08 -0500

    beginning of register-machine to C translation

--- compiler/register2c.scm
+++ compiler/register2c.scm
@@ -1,13 +1,18 @@
 
-(define (assign? inst) (tagged-list? 'assign inst))
-(define (goto? inst) (tagged-list? 'goto inst))
-(define (test? inst) (tagged-list? 'test inst))
-(define (save? inst) (tagged-list? 'save inst))
-(define (restore? inst) (tagged-list? 'restore inst))
+(load-with-macros "ch5-syntax.scm")
 
-(define (label? exp) (tagged-list? 'label exp))
-(define (const? exp) (tagged-list? 'const exp))
-(define (reg? exp) (tagged-list? 'reg exp))
+(define (assign? inst) (tagged-list? inst 'assign))
+(define (goto? inst) (tagged-list? inst 'goto))
+(define (test? inst) (tagged-list? inst 'test))
+(define (save? inst) (tagged-list? inst 'save))
+(define (restore? inst) (tagged-list? inst 'restore))
+(define (perform? inst) (tagged-list? inst 'perform))
+
+(define (label-stmt? inst) (symbol? inst))
+
+(define (label-exp? exp) (tagged-list? exp 'label))
+(define (const? exp) (tagged-list? exp 'const))
+(define (reg? exp) (tagged-list? exp 'reg))
 
 (define (make-string x)
   (cond ((string? x) x)
@@ -15,12 +20,40 @@
         ((number? x) (number->string x))
         (else "?")))
 
-(define (assign->c inst)
-  (if (tagged-list? 'op (caddr inst))
-      (append (list (register->c (cadr inst)) "=")
-              )))
+(define (stmt->c inst)
+  (cond ((assign? inst) (assign->c inst))
+        ((label-stmt? inst) (label->c inst))
+        ((goto? inst) (goto->c inst))
+        (else (list "???"))
+        ))
 
-(define (register->c name) (list "reg[" name "]"))
+(define (exp->c exp)
+  (cond ((label-exp? exp) (label->c exp))
+        ((reg? exp) (reg->c exp))
+        ((const? exp) (const->c exp))
+        ))
+
+(define (goto->c inst)
+  (list "GOTO(" (exp->c (cadr inst)) ")"))
+
+(define (label->c inst)
+  (if (symbol? inst)
+      (list "case " inst ":\n")
+      (list "label2obj(" (cadr inst) ")")))
+
+(define (assign->c inst)
+  (if (tagged-list? (caddr inst) 'op)
+      (append (list (reg->c (cadr inst)) "=")
+              (opcall->c (caddr inst) (cdddr inst)))
+      (append (list (reg->c (cadr inst)) "=")
+              (exp->c (caddr inst)))))
+
+(define (opcall->c op args)
+  (append (list (cadr op) "(")
+          (interleave "," args)
+          (list ")")))
+
+(define (reg->c name) (list "reg[" name "]"))
 
 (define (const->c const)
   (cond ((number? const) const)
@@ -36,4 +69,3 @@
       (append (list "cons(" (car lst) ",")
               (list->cons-calls (cdr lst))
               (list ")"))))
-
